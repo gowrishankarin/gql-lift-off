176da324f707da2b51600aa3f9b65ca14eed8280
Gowri Shankar <gowri.turing@apollo.io>
2024-02-25 17:10:50 +0530

    GS/INIT: App push

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+const typeDefs = require("./schema");
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
